9e5f4694e461f1705decf9f2889fd5cb1961f134
Bala <anba@gft.com>
2020-02-24 19:02:43 +0100

    Simple implementation GQL server using Express and Apollo

---
+++ src/data/discoverers.ts
@@ -0,0 +1,5 @@
+export const discoverers = [
+    { id: 1, firstName: 'Maria', lastName: 'Skłodowska-Curie' },
+    { id: 2, firstName: 'Martin', lastName: 'Klaproth' },
+    { id: 3, firstName: 'William', lastName: 'Gregor' },
+];
---
+++ src/data/elements.ts
@@ -0,0 +1,5 @@
+export const elements = [
+    { id: 1, discovererId: 1, atomicNumber: 1, atomicMass: 1.00794, symbol: 'H', name: 'Hydrogen' },
+    { id: 2, discovererId: 2, atomicNumber: 2, atomicMass: 4.0026, symbol: 'He', name: 'Helium' },
+    { id: 3, discovererId: 2, atomicNumber: 3, atomicMass: 6.941, symbol: 'Li', name: 'Lithium' }
+];
